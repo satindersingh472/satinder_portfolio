6382a4099be88a038095373e168889dd8348b57a
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-10 01:05:47 -0600

    added vscode

---
+++ src/components/logoSatinder.vue
@@ -0,0 +1,37 @@
+<template>
+    <div>
+<h2>{S@t<span class="blue">i</span>n<img class="vscode" src="@/assets/vscode.png"/>e<span class="r">r</span>_S<span class="yellow">i</span>n<span class="g">G</span>h}</h2>
+      
+    </div>
+</template>
+
+<script>
+    export default {
+        
+    }
+</script>
+
+<style lang="scss" scoped>
+.blue{
+    color: #4b8bbe;
+}
+.yellow{
+    color: #FFD43B;
+}
+.g{
+  background: linear-gradient(#4285F4,#DB4437,#F4B400,#4285F4,#0F9D58);
+  background-clip: text;
+  -webkit-text-fill-color: transparent;
+}
+.r{
+  color: #61DBFB;
+}
+.vscode{
+    height: 25px;
+    transform: rotate(180deg);
+}
+h2,.blue,.yellow,.g,.r{
+    font-weight: bold;
+    font-size: 2rem;
+  }
+</style>
--- src/components/navbarDesktop.vue
+++ src/components/navbarDesktop.vue
@@ -2,8 +2,8 @@
   <div>
     <div class="entire_navbar">
       <div class="name_container">
-        <h2>{S@t<span class="blue">i</span>nde<span class="r">r</span>_S<span class="yellow">i</span>n<span class="g">G</span>h}</h2>
-      </div>
+       <logo-satinder></logo-satinder>
+        </div>
       <div class="links">
         <h2>Projects</h2>
         <h2>Tools&Tech</h2>
@@ -14,7 +14,12 @@
 </template>
 
 <script>
-export default {}
+import LogoSatinder from "@/components/logoSatinder.vue";
+export default {
+  components: {
+    LogoSatinder,
+  },
+}
 </script>
 
 <style lang="scss" scoped>
@@ -33,25 +38,12 @@ img{
   height: 8vh;
   grid-template-columns: 1fr 1fr;
 }
-.blue{
-    color: #4b8bbe;
-}
-.yellow{
-    color: #FFD43B;
-}
-.g{
-  background: linear-gradient(#4285F4,#DB4437,#F4B400,#4285F4,#0F9D58);
-  background-clip: text;
-  -webkit-text-fill-color: transparent;
-}
-.r{
-  color: #61DBFB;
-}
+
 .name_container {
   display: grid;
   align-items: center;
   justify-items: start;
-  h2,.blue,.yellow,.g,.r{
+  h2{
     font-weight: bold;
     font-size: 2rem;
   }
