f7169128db0d0a0f23c499b42d95665000b95b17
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-10-12 21:18:23 -0600

    working on contact form

--- src/views/contactPage.vue
+++ src/views/contactPage.vue
@@ -1,22 +1,22 @@
 <template>
-  <div>
+  <div class="page">
     <div class="form">
       <div class="form_heading">
         <h2>Get In Touch</h2>
       </div>
       <div class="form_fields">
-      <form action="https://formspree.io/f/mjvzoeoy" method="post">
+      <form class="actual_form" action="https://formspree.io/f/mjvzoeoy" method="post">
         <div class="one_field">
           <p>First Name<span class="star">*</span></p>
-          <input type="text" ref="first_name" name="firstname" placeholder="First Name" />
+          <input type="text" ref="first_name" name="firstname"/>
         </div>
         <div class="one_field">
             <p>Last Name<span class="star">*</span></p>
-            <input type="text" ref="last_name" name="lastname" placeholder="Last Name"/>
+            <input type="text" ref="last_name" name="lastname"/>
         </div>
         <div class="one_field">
             <p>Email<span class="star">*</span></p>
-            <input type="email" ref="email" name="email" placeholder="email">
+            <input type="email" ref="email" name="email">
         </div>
         <div class="one_field">
             <p>Message<span class="star">*</span></p>
@@ -38,11 +38,54 @@ export default {}
   padding:0px;
   margin: 0px;
 }
+.page{
+  display: grid;
+  place-items: center;
+  margin: 10vh 0px;
+}
 .form{
   display: grid;
   place-items: center;
-  background: #FFD43B;
-  height: 100vh;
+  width: 100%;
+  .form_heading{
+    h2{
+      font-size:2rem;
+      font-weight:bold;
+    }
+  }
+  .form_fields{
+    width:100%;
+    display:grid;
+    input[type=text],input[type=email],textarea{
+      padding: 20px 0px;
+      background: #FFD43B;
+      border:none;
+    }
+  }
+}
+.actual_form{
+  display:grid;
+  gap: 10px;
+   input[type=submit]{
+      display: grid;
+      justify-self:center;
+      padding: 10px;
+      margin:10px;
+    }
+}
+.one_field{
+  display: grid;
+  gap: 10px;
+  width: 100%;
+  p{
+    font-size: 1.3rem;
+  }
+}
+
+@media only screen and (min-width:800px){
+  .form{
+    width:60vw;
+  }
 }
 
 </style>
