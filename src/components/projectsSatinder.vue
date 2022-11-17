2b07df82b0e402d109974e551817e718d3bca97d
satinder <satindersingh472@gmail.com>
2022-11-17 15:52:16 -0700

    working_on_projects

--- src/components/projectsSatinder.vue
+++ src/components/projectsSatinder.vue
@@ -2,10 +2,20 @@
   <div>
     <v-container fluid>
       <v-row justify="center" >
-        <h2 class="grey--text text--darken-2 my-10 text-h3 text-sm-h2 text-md-h1" >___Projects</h2>
+        <h2 class="indigo--text text--darken-2 my-10 text-h3 text-sm-h2 text-md-h1" >___Projects</h2>
       </v-row>
-  <v-row>
-    
+  <v-row justify="center">
+    <v-col cols="12" sm="8" md="8" lg="6" class="text-center">
+      <v-toolbar  flat>
+        <a  class="text-h3 black--text " href="https://myride.ml" target="blank">MyRide</a>
+      </v-toolbar>
+      <v-card-text>
+        <v-row>
+          <v-col></v-col>
+        </v-row>
+      </v-card-text>
+    </v-col>
+
   </v-row>
     </v-container>
   </div>
