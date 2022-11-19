33295b3de46a849e49b4838129ecc5fef203a8d6
satinder <satindersingh472@gmail.com>
2022-11-19 15:08:36 -0700

    working_on_signup

--- src/components/projectsSatinder.vue
+++ src/components/projectsSatinder.vue
@@ -9,22 +9,24 @@
           <p class="text-h2 font-weight-bold" style="align-self:end;">
             MyRide
           </p>
-          <p class="grey lighten-3 pa-5" style="align-self:center;">
+          <p class="grey lighten-3 pa-5 text-h6" style="align-self:center;">
             A website for posting and booking rides from one place to another. See upcoming rides, filter rides by city name,
             post rides in advance, user can act as a driver or passenger at different times, confirm and book rides easily. It is kind of a marketplace for traveling.
           </p>
-          <p class="grey--text text--darken-2" >Technologies used: VueJs, Vuetify, Javascript, Python, Flask, Mariadb. </p>
-          <v-row style="display:grid;place-items:center;grid-auto-flow:column;" align-content="start" >
+          <p class="grey--text text--darken-3 ma-5 text-h6"><span class="indigo--text text--darken-4">Technologies used:</span> VueJs, Vuetify, Python, Flask, Mariadb, Dbeaver, Git and Github. </p>
+          <div align-content="start" >
             <v-btn class="ma-5" elevation="5" >Live website</v-btn>
             <v-btn class="ma-5 " elevation="5">Github Repo</v-btn>
             <router-link to="/myride" class="text-decoration-none" ><v-btn class="ma-5" elevation="5" >Case study</v-btn></router-link>
-          </v-row>
+          </div>
         </v-col>
-        <v-col cols="12" md="6" >
+        <v-col cols="12" md="6" style="display:grid;place-items:center;" >
           <v-img
             src="@/assets/mockup_myride.png"
-            max-height="800"
-            max-width="800"
+            max-height="500"
+            max-width="600"
+            width="100%"
+            contain
           ></v-img>
         </v-col>
       </v-row>
--- src/views/caseMyride.vue
+++ src/views/caseMyride.vue
@@ -20,22 +20,24 @@
         <p class="text-h4 indigo--text text--darken-4">Technologies used</p>
       </v-row>
       <v-row class="ma-10" justify="center">
-        <v-col cols="6" >
-          <p class="text-h5 grey--text text--darken-2" >HTML5, CSS3, Javascript, Vuejs, Python, Flask, Mariadb, Dbeaver, SQL, Git, Github and Vuetify. </p>
+        <v-col cols="12" lg="6" >
+          <p class="text-h5" >It is a full stack application. Front end was designed and developed with Vuejs. Back end developed with Python and Flask. Database with the help of Mariadb and Dbeaver. Git is 
+            is used as a version control system and Github for hosting the source code. The website is hosted with oracle cloud server.</p>
         </v-col>
       </v-row>
       <v-row justify="center" class="my-10">
         <v-col cols="12" lg="6" style="display: grid; place-items: center;">
-          <v-row>
+          <v-row >
             <v-img
               src="@/assets/login_myride.png"
-              max-height="500"
               contain
+              width="100%"
+              max-height="500"
               max-width="500"
             ></v-img>
           </v-row>
         </v-col>
-        <v-col cols="12" lg="6" align-self="center">
+        <v-col cols="12" lg="6" align-self="center" >
           <p class="text-h5">
             1. Signup with valid email and the user will recieve an email
             verification.
@@ -49,13 +51,12 @@
           </p>
         </v-col>
       </v-row>
-      <v-row class="my-10">
+      <v-row class="my-10" justify="center">
         <v-col
           cols="12"
           lg="6"
           style="display: grid; place-items: center;"
-          order-sm="1"
-          order-md="2"
+       
         >
           <v-img
             src="@/assets/search_upcoming.png"
@@ -65,18 +66,18 @@
           ></v-img>
         </v-col>
 
-        <v-col cols="12" lg="6" align-self="center" order-md="1" order-sm="2">
+        <v-col cols="12" lg="6" align-self="center" >
           <p class="text-h5">
             1.Search rides from one city to another city.
           </p>
-          <p class="text-h5">2. Discover upcoming rides.</p>
+          <p class="text-h5">2. Discover upcoming rides along with information about the rides.</p>
           <p class="text-h5">
             3. Book ride from the same page with just one-click.
           </p>
         </v-col>
       </v-row>
       <v-row>
-        <v-col cols="12" lg="6" style="display: grid; place-items: center;">
+        <v-col cols="12" lg="6">
           <v-img
             src="@/assets/profile_ride.png"
             contain
@@ -84,7 +85,7 @@
             max-width="500"
           ></v-img>
         </v-col>
-        <v-col align-self="center" cols="12" lg="6">
+        <v-col align-self="center" cols="12" lg="6" >
           <p class="text-h5">
             1.Clients can change any information related to profile easily.
           </p>
@@ -98,8 +99,7 @@
           cols="12"
           lg="6"
           style="display: grid; place-items: center;"
-          order-md="2"
-          order-sm="1"
+
         >
           <v-img
             src="@/assets/posted_booking.png"
@@ -108,7 +108,7 @@
             max-width="500"
           ></v-img>
         </v-col>
-        <v-col order-md="1" order-sm="2" align-self="center">
+        <v-col  align-self="center">
           <p class="text-h5">
             1.After posting rides it is easy to access the bookings.
           </p>
