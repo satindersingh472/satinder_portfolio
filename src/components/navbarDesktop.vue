6be4455049b021b97228705ce7c1478e37bd21bb
satinder <satindersingh472@gmail.com>
2022-11-20 17:12:35 -0700

    working

--- src/components/navbarDesktop.vue
+++ src/components/navbarDesktop.vue
@@ -11,7 +11,7 @@
         <v-row justify="center" >
       <router-link class="text-decoration-none indigo--text text--darken-3 text-h5 mx-10" to="/projects_satinder">Projects</router-link>
       <router-link class="text-decoration-none indigo--text text--darken-3 text-h5 mx-10" to="/contact">Contact</router-link>
-      <a class="text-decoration-none indigo--text text--darken-3 text-h5 mx-10" href="https://www.github.com/satindersingh472">Github</a>
+      <a class="text-decoration-none indigo--text text--darken-3 text-h5 mx-10" target="blank" href="https://www.github.com/satindersingh472">Github</a>
         </v-row>
       </v-col>
     </v-app-bar>
