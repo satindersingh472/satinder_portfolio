f7ec1271cf1b6bc5f5d0ead64bea405057d30200
satindersingh772 <92008199+satindersingh772@users.noreply.github.com>
2022-11-23 13:27:04 -0700

    worked_on_tools

--- src/components/toolKit.vue
+++ src/components/toolKit.vue
@@ -5,45 +5,66 @@
         <p class="indigo--text text--darken-4 my-10 text-h2">
           Tools
         </p>
+        <p class="text-h6" >
+          I use these tools and technologies for my projects but every project
+          needs different types of technologies and I can learn new technologies
+          required for the project.
+        </p>
       </v-row>
-      <v-card flat class="rounded-lg grey lighten-2 ">
-        <v-row justify="center" >
-          <v-col   style="display:grid;place-items:center;" >
+      <v-card flat class="rounded-lg grey lighten-2">
+        <v-row justify="center">
+          <v-col style="display: grid; place-items: center;">
             <v-icon class="deep-orange--text" size="75">
               mdi-language-html5
             </v-icon>
-            <p class="text-h6" >HTML</p>
+            <p class="text-h6">HTML</p>
           </v-col>
-          <v-col style="display:grid;place-items:center;" >
-            <v-icon class="blue--text" size="75" >mdi-language-css3</v-icon>
+          <v-col style="display: grid; place-items: center;">
+            <v-icon class="blue--text" size="75">mdi-language-css3</v-icon>
             <p class="text-h6">CSS3</p>
           </v-col>
-          <v-col style="display:grid;place-items:center;" >
-            <v-icon class="yellow--text text--darken-3 " size="75" >mdi-language-javascript</v-icon>
+          <v-col style="display: grid; place-items: center;">
+            <v-icon class="yellow--text text--darken-3" size="75">
+              mdi-language-javascript
+            </v-icon>
             <p class="text-h6">Javascript</p>
           </v-col>
-          <v-col style="display:grid;place-items:center;" >
-            <v-icon class="green--text" size="75" >mdi-vuejs</v-icon>
+          <v-col style="display: grid; place-items: center;">
+            <v-icon class="green--text" size="75">mdi-vuejs</v-icon>
             <p class="text-h6">VueJs</p>
           </v-col>
-          <v-col style="display:grid;place-items:center;" >
-            <v-icon class="blue--text" size="75" >mdi-language-python</v-icon>
+          <v-col style="display: grid; place-items: center;">
+            <v-icon class="blue--text" size="75">mdi-language-python</v-icon>
             <p class="text-h6">Python</p>
           </v-col>
-          <v-col style="display:grid;place-items:center;" >
-            <v-icon class="blue--text" size="75" >mdi-vuetify</v-icon>
+          <v-col style="display: grid; place-items: center;">
+            <v-icon class="blue--text" size="75">mdi-vuetify</v-icon>
             <p class="text-h6">Vuetify</p>
           </v-col>
-          <v-col style="display:grid;place-items:center;" >
-            <v-img src="@/assets/flask.png" contain max-height="75" max-width="75" width="100%" ></v-img>
+          <v-col style="display: grid; place-items: center;">
+            <v-img
+              src="@/assets/flask.png"
+              contain
+              max-height="75"
+              max-width="75"
+              width="100%"
+            ></v-img>
             <p class="text-h6">Flask</p>
           </v-col>
-          <v-col style="display:grid;place-items:center;" >
-        <v-icon class="grey--text text--darken-3 " size="75" >mdi-database</v-icon>
+          <v-col style="display: grid; place-items: center;">
+            <v-icon class="grey--text text--darken-3" size="75">
+              mdi-database
+            </v-icon>
             <p class="text-h6">Maridb</p>
           </v-col>
-          <v-col style="display:grid;place-items:center;" >
-            <v-img src="@/assets/postman.webp" contain max-height="75" max-width="75" width="100%"  ></v-img>
+          <v-col style="display: grid; place-items: center;">
+            <v-img
+              src="@/assets/postman.webp"
+              contain
+              max-height="75"
+              max-width="75"
+              width="100%"
+            ></v-img>
             <p class="text-h6">Postman</p>
           </v-col>
         </v-row>
