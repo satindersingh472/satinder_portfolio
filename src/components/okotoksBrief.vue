863a2337c318e7f5fb8d49552c52edfb3c2f9793
satinder <satindersingh472@gmail.com>
2022-11-22 23:05:00 -0700

    working_okotoks_case

--- src/components/okotoksBrief.vue
+++ src/components/okotoksBrief.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    <v-row justify="end">
+    <v-row justify="end" class="my-10" >
       <v-col cols="12" md="5" style="display: grid; justify-items: start;">
         <p class="text-h2 font-weight-bold" style="align-self: end;">
           Okotoks Pizza
@@ -14,12 +14,29 @@
           <span class="indigo--text text--darken-4">Technologies used:</span>
         HTML, CSS, Scss.
         </p>
+               <div align-content="start">
+          <a
+            href="https://www.okotokspizza.ml"
+            target="blank"
+            class="text-decoration-none"
+          >
+            <v-btn class="ma-5" elevation="5">Live website</v-btn>
+          </a>
+          <a href="https://github.com/satindersingh472/okotoksPizza_lab" target="blank" class="text-decoration-none" >
+            <v-btn class="ma-5" elevation="5">Github Repo</v-btn>
+          </a>
+          <router-link to="/foodie" class="text-decoration-none">
+            <v-btn class="ma-5" elevation="5">Case study</v-btn>
+          </router-link>
+        </div>
       </v-col>
       <v-col
         cols="12"
         md="6"
         style="display: grid; place-items: center;"
-      ></v-col>
+      >
+      <v-img src="@/assets/okotoks_homepage.png" contain width="100%" max-height="500" max-width="500" ></v-img>
+      </v-col>
     </v-row>
   </div>
 </template>
